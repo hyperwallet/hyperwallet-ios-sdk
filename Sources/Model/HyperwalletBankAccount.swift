faa8704d1fb6cb09d69d80d6520c066be3cf90ef
Oleksii Lubianyi <Oleksii_Lubianyi@epam.com>
2019-05-15 19:09:36 +0300

    Implementation for HW-52629: Update HyperwalletBankAccount in hyperwallet-ios-sdk

--- Sources/Model/HyperwalletBankAccount.swift
+++ Sources/Model/HyperwalletBankAccount.swift
@@ -46,6 +46,15 @@ public final class HyperwalletBankAccount: HyperwalletTransferMethod {
         case ownCompany = "OWN_COMPANY"
     }
 
+    /// Representation of the user's profile type.
+    ///
+    /// - business: Business.
+    /// - individual: Individual.
+    public enum ProfileType: String {
+        case business = "BUSINESS"
+        case individual = "INDIVIDUAL"
+    }
+
     /// A helper class to build the `HyperwalletBankAccount` instance.
     public final class Builder {
         private var storage = [String: AnyCodable]()
@@ -72,6 +81,24 @@ public final class HyperwalletBankAccount: HyperwalletTransferMethod {
             storage[TransferMethodField.profileType.rawValue] = AnyCodable(value: transferMethodProfileType)
         }
 
+        /// Sets profile address line 1
+        ///
+        /// - Parameter addressLine1: address line 1
+        /// - Returns: a self `HyperwalletBankAccount.Builder` instance.
+        public func addressLine1(_ addressLine1: String) -> Builder {
+            storage[TransferMethodField.addressLine1.rawValue] = AnyCodable(value: addressLine1)
+            return self
+        }
+
+        /// Sets profile address line 2
+        ///
+        /// - Parameter addressLine2: address line 2
+        /// - Returns: a self `HyperwalletBankAccount.Builder` instance.
+        public func addressLine2(_ addressLine2: String) -> Builder {
+            storage[TransferMethodField.addressLine2.rawValue] = AnyCodable(value: addressLine2)
+            return self
+        }
+
         /// Sets the bank account number, IBAN or equivalent. If you are providing an IBAN, the first two
         /// letters of the IBAN must match the `transferMethodCountry`
         ///
@@ -79,15 +106,6 @@ public final class HyperwalletBankAccount: HyperwalletTransferMethod {
         /// - Returns: a self `HyperwalletBankAccount.Builder` instance.
         public func bankAccountId(_ bankAccountId: String) -> Builder {
             storage[TransferMethodField.bankAccountId.rawValue] = AnyCodable(value: bankAccountId)
-            return self
-        }
-
-        /// Sets the bank code or equivalent (e.g. BIC/SWIFT code).
-        ///
-        /// - Parameter bankId: the bank code or equivalent (e.g. BIC/SWIFT code)
-        /// - Returns: a self `HyperwalletBankAccount.Builder` instance.
-        public func bankId(_ bankId: String) -> Builder {
-            storage[TransferMethodField.bankId.rawValue] = AnyCodable(value: bankId)
             return self
         }
 
@@ -109,12 +127,192 @@ public final class HyperwalletBankAccount: HyperwalletTransferMethod {
             return self
         }
 
+        /// Sets the bank code or equivalent (e.g. BIC/SWIFT code).
+        ///
+        /// - Parameter bankId: the bank code or equivalent (e.g. BIC/SWIFT code)
+        /// - Returns: a self `HyperwalletBankAccount.Builder` instance.
+        public func bankId(_ bankId: String) -> Builder {
+            storage[TransferMethodField.bankId.rawValue] = AnyCodable(value: bankId)
+            return self
+        }
+
+        /// Sets the bank name.
+        ///
+        /// - Parameter bankName: the bank name
+        /// - Returns: a self `HyperwalletBankAccount.Builder` instance.
+        public func bankName(_ bankName: String) -> Builder {
+            storage[TransferMethodField.bankName.rawValue] = AnyCodable(value: bankName)
+            return self
+        }
+
         /// Sets the bank account branch code, transit number, routing number or equivalent.
         ///
         /// - Parameter branchId: The branch code, transit number, routing number or equivalent.
         /// - Returns: a self `HyperwalletBankAccount.Builder` instance.
         public func branchId(_ branchId: String) -> Builder {
             storage[TransferMethodField.branchId.rawValue] = AnyCodable(value: branchId)
+            return self
+        }
+
+        /// Sets the bank account branch name.
+        ///
+        /// - Parameter branchName: The branch name.
+        /// - Returns: a self `HyperwalletBankAccount.Builder` instance.
+        public func branchName(_ branchName: String) -> Builder {
+            storage[TransferMethodField.branchName.rawValue] = AnyCodable(value: branchName)
+            return self
+        }
+
+        /// Sets the profile business contact role.
+        ///
+        /// - Parameter businessContactRole: The profile business contact role.
+        /// - Returns: a self `HyperwalletBankAccount.Builder` instance.
+        public func businessContactRole(_ businessContactRole: String) -> Builder {
+            storage[TransferMethodField.businessContactRole.rawValue] = AnyCodable(value: businessContactRole)
+            return self
+        }
+
+        /// Sets the profile business name.
+        ///
+        /// - Parameter businessName: The profile business name.
+        /// - Returns: a self `HyperwalletBankAccount.Builder` instance.
+        public func businessName(_ businessName: String) -> Builder {
+            storage[TransferMethodField.businessName.rawValue] = AnyCodable(value: businessName)
+            return self
+        }
+
+        /// Sets the profile business registration country.
+        ///
+        /// - Parameter businessRegistrationCountry: The profile business registration country.
+        /// - Returns: a self `HyperwalletBankAccount.Builder` instance.
+        public func businessRegistrationCountry(_ businessRegistrationCountry: String) -> Builder {
+            storage[TransferMethodField.businessRegistrationCountry.rawValue] = AnyCodable(value: businessRegistrationCountry)
+            return self
+        }
+
+        /// Sets the profile business registration id.
+        ///
+        /// - Parameter businessRegistrationId: The profile business registration id.
+        /// - Returns: a self `HyperwalletBankAccount.Builder` instance.
+        public func businessRegistrationId(_ businessRegistrationId: String) -> Builder {
+            storage[TransferMethodField.businessRegistrationId.rawValue] = AnyCodable(value: businessRegistrationId)
+            return self
+        }
+
+        /// Sets the profile business registration state/province.
+        ///
+        /// - Parameter businessRegistrationStateProvince: The profile business registration state/province.
+        /// - Returns: a self `HyperwalletBankAccount.Builder` instance.
+        public func businessRegistrationStateProvince(_ businessRegistrationStateProvince: String) -> Builder {
+            storage[TransferMethodField.businessRegistrationStateProvince.rawValue] = AnyCodable(value: businessRegistrationStateProvince)
+            return self
+        }
+
+        /// Sets the profile business type.
+        ///
+        /// - Parameter businessType: The profile business type.
+        /// - Returns: a self `HyperwalletBankAccount.Builder` instance.
+        public func businessType(_ businessType: String) -> Builder {
+            storage[TransferMethodField.businessType.rawValue] = AnyCodable(value: businessType)
+            return self
+        }
+
+        /// Sets the profile country.
+        ///
+        /// - Parameter country: The profile country.
+        /// - Returns: a self `HyperwalletBankAccount.Builder` instance.
+        public func country(_ country: String) -> Builder {
+            storage[TransferMethodField.country.rawValue] = AnyCodable(value: country)
+            return self
+        }
+
+        /// Sets the profile city.
+        ///
+        /// - Parameter city: The profile city.
+        /// - Returns: a self `HyperwalletBankAccount.Builder` instance.
+        public func city(_ city: String) -> Builder {
+            storage[TransferMethodField.city.rawValue] = AnyCodable(value: city)
+            return self
+        }
+
+        /// Sets the profile date of birth.
+        ///
+        /// - Parameter dateOfBirth: The profile date of birth.
+        /// - Returns: a self `HyperwalletBankAccount.Builder` instance.
+        public func dateOfBirth(_ dateOfBirth: String) -> Builder {
+            storage[TransferMethodField.dateOfBirth.rawValue] = AnyCodable(value: dateOfBirth)
+            return self
+        }
+
+        /// Sets the profile first name.
+        ///
+        /// - Parameter firstName: The profile first name.
+        /// - Returns: a self `HyperwalletBankAccount.Builder` instance.
+        public func firstName(_ firstName: String) -> Builder {
+            storage[TransferMethodField.firstName.rawValue] = AnyCodable(value: firstName)
+            return self
+        }
+
+        /// Sets the profile government id.
+        ///
+        /// - Parameter governmentId: The profile government id.
+        /// - Returns: a self `HyperwalletBankAccount.Builder` instance.
+        public func governmentId(_ governmentId: String) -> Builder {
+            storage[TransferMethodField.governmentId.rawValue] = AnyCodable(value: governmentId)
+            return self
+        }
+
+        /// Sets the profile last name.
+        ///
+        /// - Parameter lastName: The profile last name.
+        /// - Returns: a self `HyperwalletBankAccount.Builder` instance.
+        public func lastName(_ lastName: String) -> Builder {
+            storage[TransferMethodField.lastName.rawValue] = AnyCodable(value: lastName)
+            return self
+        }
+
+        /// Sets the profile mobile number.
+        ///
+        /// - Parameter mobileNumber: The profile mobile number.
+        /// - Returns: a self `HyperwalletBankAccount.Builder` instance.
+        public func mobileNumber(_ mobileNumber: String) -> Builder {
+            storage[TransferMethodField.mobileNumber.rawValue] = AnyCodable(value: mobileNumber)
+            return self
+        }
+
+        /// Sets the profile phone number.
+        ///
+        /// - Parameter phoneNumber: The profile phone number.
+        /// - Returns: a self `HyperwalletBankAccount.Builder` instance.
+        public func phoneNumber(_ phoneNumber: String) -> Builder {
+            storage[TransferMethodField.phoneNumber.rawValue] = AnyCodable(value: phoneNumber)
+            return self
+        }
+
+        /// Sets the profile postal code.
+        ///
+        /// - Parameter postalCode: The profile postal code.
+        /// - Returns: a self `HyperwalletBankAccount.Builder` instance.
+        public func postalCode(_ postalCode: String) -> Builder {
+            storage[TransferMethodField.postalCode.rawValue] = AnyCodable(value: postalCode)
+            return self
+        }
+
+        /// Sets the profile type (e.g. individual or business).
+        ///
+        /// - Parameter profileType: The `ProfileType`.
+        /// - Returns: a self `HyperwalletBankAccount.Builder` instance.
+        public func profileType(_ profileType: ProfileType) -> Builder {
+            storage[TransferMethodField.profileType.rawValue] = AnyCodable(value: profileType.rawValue)
+            return self
+        }
+
+        /// Sets the profile state/province.
+        ///
+        /// - Parameter stateProvince: The profile state/province.
+        /// - Returns: a self `HyperwalletBankAccount.Builder` instance.
+        public func stateProvince(_ stateProvince: String) -> Builder {
+            storage[TransferMethodField.stateProvince.rawValue] = AnyCodable(value: stateProvince)
             return self
         }
 
