1780f258d8f3e22047789a4dff9cd2006fe2c2e2
malaw-hw <51347548+malaw-hw@users.noreply.github.com>
2019-07-12 13:09:39 -0700

    Task/hw 53712 add accessors to bank account bank card paypal account (#41)
    * Added accessors for HyperwalletBankAccount
    
    * Added accessors for HyperwalletBankCard
    
    * Added accessors for HyperwalletPayPalAccount
    
    * HW-53713: Replace All the Usages of getField Method with Accessors
    
    * Moved `profileType` into `HyperwalletTransferMethod`
    
    * Replaced `getField` calls with appropriate properties in new tests
    
    * Provided fixes according to PR comments
    
    * HW-54157: Add getField(String) in HyperwalletTransferMethod and HyperwalletUser (#37)
    
    * HW-54157: Add getField(String) in HyperwalletTransferMethod and HyperwalletUser
    
    * Fixed tests errors and warnings
    
    * Fixed test coverage
    
    * fixed swiftlint warning

--- Sources/Model/TransferMethod/HyperwalletPrepaidCard.swift
+++ Sources/Model/TransferMethod/HyperwalletPrepaidCard.swift
@@ -29,32 +29,32 @@ public final class HyperwalletPrepaidCard: HyperwalletTransferMethod {
 
     /// The prepaid card's type
     public var cardType: String? {
-        return getField(fieldName: .cardType)
+        return getField(TransferMethodField.cardType.rawValue)
     }
 
     /// The prepaid card's package
     public var cardPackage: String? {
-        return getField(fieldName: .cardPackage)
+        return getField(TransferMethodField.cardPackage.rawValue)
     }
 
     /// The prepaid card's number
     public var cardNumber: String? {
-        return getField(fieldName: .cardNumber)
+        return getField(TransferMethodField.cardNumber.rawValue)
     }
 
     /// The prepaid card's brand
     public var cardBrand: String? {
-        return getField(fieldName: .cardBrand)
+        return getField(TransferMethodField.cardBrand.rawValue)
     }
 
     /// The prepaid card's expiry date
     public var dateOfExpiry: String? {
-        return getField(fieldName: .dateOfExpiry)
+        return getField(TransferMethodField.dateOfExpiry.rawValue)
     }
 
     /// The prepaid card's user token (instant issue cards only)
     public var userToken: String? {
-        return getField(fieldName: .userToken)
+        return getField(TransferMethodField.userToken.rawValue)
     }
 
     /// A helper class to build the `HyperwalletPrepaidCard` instance.
