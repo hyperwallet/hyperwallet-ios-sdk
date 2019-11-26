11c4ac4dc86da5dc70cc9ed9d5bcb240e34d5f83
Arun <asomasundaram@paypal.com>
2019-11-27 00:33:38 +0530

    Updated script file.

--- scripts/travisPublishDocs.sh
+++ scripts/travisPublishDocs.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 # Set identity
-git config --global user.email "travis@travis-ci.org"
-git config --global user.name "Travis"
+git config --global user.email "travis-ci-hyperwallet@paypal.com"
+git config --global user.name "travis-ci-hyperwallet"
 
 # Publish docs
 mkdir ../gh-pages
@@ -11,7 +11,7 @@ cd ../gh-pages
 
 # Add branch
 git init
-git remote add origin https://${GH_TOKEN}@github.com/hyperwallet/python-sdk.git > /dev/null
+git remote add origin https://${CI_USER_TOKEN}@github.com/hyperwallet/hyperwallet-ios-ui-sdk.git > /dev/null
 git checkout -B gh-pages
 
 # Push generated files
