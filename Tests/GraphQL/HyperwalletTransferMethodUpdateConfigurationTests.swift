63a1256b75d56e36ac5cd3c34635f82f8febcf30
aparthibanpaypal <71116197+aparthibanpaypal@users.noreply.github.com>
2020-12-09 13:08:49 +0530

    Added test cases for Edit Transfer Methods (Bank Card, Bank Account and Paypal)

--- Tests/GraphQL/HyperwalletTransferMethodUpdateConfigurationTests.swift
+++ Tests/GraphQL/HyperwalletTransferMethodUpdateConfigurationTests.swift
@@ -32,7 +32,7 @@ class HyperwalletTransferMethodUpdateConfigurationTests: XCTestCase {
     }
 
     //swiftlint:disable function_body_length
-    func testRetrieveTransferMethodUpdateConfigurationFields_success() {
+    func testRetrieveTransferMethodUpdateConfigurationFields_BankAccount() {
         // Given
         let request = setUpTransferMethodUpdateConfigurationRequest("TransferMethodUpdateConfigurationFieldsResponse")
         HyperwalletTestHelper.setUpMockServer(request: request)
@@ -102,6 +102,126 @@ class HyperwalletTransferMethodUpdateConfigurationTests: XCTestCase {
         XCTAssertEqual(postalCode?.validationMessage?.pattern,
                        "is invalid length or format.")
         XCTAssertEqual(postalCode?.fieldValueMasked, false)
+    }
+
+    func testRetrieveTransferMethodUpdateConfigurationFields_Paypal() {
+        // Given
+        let request = setUpTransferMethodUpdateConfigurationRequest(
+            "TransferMethodUpdateConfigurationFieldsPaypalResponse")
+        HyperwalletTestHelper.setUpMockServer(request: request)
+
+        let expectation = self.expectation(description:
+            "Retrieve update transfer method configuration fields for Paypal")
+
+        var graphQlResponse: HyperwalletTransferMethodUpdateConfigurationField?
+        var errorResponse: HyperwalletErrorType?
+        // When
+        let fieldQuery = HyperwalletTransferMethodUpdateConfigurationFieldQuery(transferMethodToken: "trm-0000002")
+
+        Hyperwallet.shared.retrieveTransferMethodUpdateConfigurationFields(request: fieldQuery) { (result, error) in
+            graphQlResponse = result
+            errorResponse = error
+            expectation.fulfill()
+        }
+
+        wait(for: [expectation], timeout: 1)
+
+        let fieldGroups = graphQlResponse?.transferMethodUpdateConfiguration()?.fieldGroups?.nodes
+
+        // Then
+        XCTAssertNil(errorResponse, "The `errorResponse` should be nil")
+        XCTAssertNotNil(graphQlResponse)
+        XCTAssertEqual(fieldGroups?.count,
+                       1,
+                       "`fieldGroups()` should be 1")
+
+        let email = fieldGroups?
+            .first(where: { $0.group == "CONTACT_INFORMATION" })?.fields?
+            .first(where: { $0.name == "email" })
+        XCTAssertNotNil(email)
+        XCTAssertEqual(email?.value, "hello@hw.com")
+        XCTAssertEqual(email?.dataType, HyperwalletDataType.text.rawValue)
+        XCTAssertEqual(email?.isRequired, true)
+        XCTAssertEqual(email?.isEditable, true)
+        XCTAssertEqual(email?.label, "Email")
+        XCTAssertEqual(email?.validationMessage?.length,
+                       "The minimum length of this field is 3 and maximum length is 200.")
+        XCTAssertEqual(email?.fieldValueMasked, false)
+    }
+
+    func testRetrieveTransferMethodUpdateConfigurationFields_BankCard() {
+        // Given
+        let request = setUpTransferMethodUpdateConfigurationRequest(
+            "TransferMethodUpdateConfigurationFieldsBankCardResponse")
+        HyperwalletTestHelper.setUpMockServer(request: request)
+
+        let expectation = self.expectation(description:
+            "Retrieve update transfer method configuration fields for Bank Card")
+
+        var graphQlResponse: HyperwalletTransferMethodUpdateConfigurationField?
+        var errorResponse: HyperwalletErrorType?
+        // When
+        let fieldQuery = HyperwalletTransferMethodUpdateConfigurationFieldQuery(transferMethodToken: "trm-0000003")
+
+        Hyperwallet.shared.retrieveTransferMethodUpdateConfigurationFields(request: fieldQuery) { (result, error) in
+            graphQlResponse = result
+            errorResponse = error
+            expectation.fulfill()
+        }
+
+        wait(for: [expectation], timeout: 1)
+
+        let fieldGroups = graphQlResponse?.transferMethodUpdateConfiguration()?.fieldGroups?.nodes
+
+        // Then
+        XCTAssertNil(errorResponse, "The `errorResponse` should be nil")
+        XCTAssertNotNil(graphQlResponse)
+        XCTAssertEqual(fieldGroups?.count,
+                       1,
+                       "`fieldGroups()` should be 1")
+
+        let cardNumber = fieldGroups?
+            .first(where: { $0.group == "ACCOUNT_INFORMATION" })?.fields?
+            .first(where: { $0.name == "cardNumber" })
+        XCTAssertNotNil(cardNumber)
+        XCTAssertEqual(cardNumber?.value, "****0006")
+        XCTAssertEqual(cardNumber?.dataType, HyperwalletDataType.number.rawValue)
+        XCTAssertEqual(cardNumber?.isRequired, true)
+        XCTAssertEqual(cardNumber?.isEditable, true)
+        XCTAssertEqual(cardNumber?.label, "Card Number")
+        XCTAssertEqual(cardNumber?.regularExpression, "^[0-9]{13,19}$")
+        XCTAssertEqual(cardNumber?.validationMessage?.length,
+                       "The minimum length of this field is 13 and maximum length is 19.")
+        XCTAssertEqual(cardNumber?.fieldValueMasked, true)
+        XCTAssertEqual(cardNumber?.mask?.defaultPattern, "#### #### #### ####")
+
+        let dateOfExpiry = fieldGroups?
+            .first(where: { $0.group == "ACCOUNT_INFORMATION" })?.fields?
+            .first(where: { $0.name == "dateOfExpiry" })
+        XCTAssertNotNil(dateOfExpiry)
+        XCTAssertEqual(dateOfExpiry?.value, "2024-10-01")
+        XCTAssertEqual(dateOfExpiry?.dataType, HyperwalletDataType.expiryDate.rawValue)
+        XCTAssertEqual(dateOfExpiry?.isRequired, true)
+        XCTAssertEqual(dateOfExpiry?.isEditable, true)
+        XCTAssertEqual(dateOfExpiry?.label, "Expiration Date")
+        XCTAssertEqual(dateOfExpiry?.regularExpression, "^[0-9]{4}-(1[0-2]|0[1-9])$")
+        XCTAssertEqual(dateOfExpiry?.validationMessage?.length,
+                       "The exact length of this field is 7.")
+        XCTAssertEqual(dateOfExpiry?.fieldValueMasked, false)
+
+        let cvv = fieldGroups?
+            .first(where: { $0.group == "ACCOUNT_INFORMATION" })?.fields?
+            .first(where: { $0.name == "cvv" })
+        XCTAssertNotNil(cvv)
+        XCTAssertEqual(cvv?.dataType, HyperwalletDataType.number.rawValue)
+        XCTAssertEqual(cvv?.isRequired, true)
+        XCTAssertEqual(cvv?.isEditable, true)
+        XCTAssertEqual(cvv?.label, "CVV (Card Security Code)")
+        XCTAssertEqual(cvv?.regularExpression, "^[0-9]{3,4}$")
+        XCTAssertEqual(cvv?.validationMessage?.length,
+                       "The minimum length of this field is 3 and maximum length is 4.")
+        XCTAssertEqual(cvv?.fieldValueMasked, false)
+        XCTAssertEqual(cvv?.mask?.defaultPattern, "###")
     }
 
     private func setUpTransferMethodUpdateConfigurationRequest(_ responseFile: String,
